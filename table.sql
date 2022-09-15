332ba38aa4eff1a18f21d601e48ab14e6391a7d1
srihardj1225 <87941490+srihardj1225@users.noreply.github.com>
2022-09-15 08:26:36 +0700

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,6 +1,11 @@
 SHOW ENGINES;
 SHOW TABLES;
 
+DESCRIBE nama_tabel;
+DESC nama_tabel;
+SHOW CREATE TABLE nama_tabel;
+
+
 
 --membuat table :
 
@@ -10,3 +15,16 @@ nama VARCHAR (100),
 harga INT,
 jumlah INT,
 ) ENGINE = InnoDB;
+
+
+ALTER TABLE barang
+
+          ADD COLUMN nama_column TEXT,
+
+          DROP COLUMN nama,
+
+          RENAME COLUMN nama TO nama_baru,
+
+          MODIFY nama VARCHAR(100) AFTER jumlah,
+
+         MODIFY nama VARCHAR(100) FIRST;
